84c4787a2087b2170cce34af3d4000ec536be2fb
Jonas Jensen <jdudal11@gmail.com>
2022-09-04 12:12:56 +0200

    Added snake logic and player input

--- Program.fsx
+++ Program.fsx
@@ -1,5 +1,10 @@
 ﻿open System
 
+module ListTools =
+    let rec ReplaceFront elm = function
+        | [] -> []
+        | front::tail -> elm::ReplaceFront front tail
+
 module ConsoleInterface =
     (* Writes a character to the screen *)
     let WriteScreen (x: char) =
@@ -42,26 +47,64 @@ module Console =
             | (x, y, w, h) -> ConsoleInterface.WriteScreen(' '); Border (x-1, y, w, h)
         Border(w-1, h-1, w-1, h-1)
 
+    (* Draws a snake recursively *)
     let rec DrawSnake = function
         | [] -> ()
         | first::tail -> ConsoleInterface.MoveCursor first; ConsoleInterface.WriteScreen '*'; DrawSnake tail
+
+    (* Clears the old snake recursively *)
+    let ClearOldSnake snake =
+        ConsoleInterface.MoveCursor snake
+        ConsoleInterface.WriteScreen ' '
 
 module Input =
     (* Returns if the program should exit or not *)
     let ShouldExit () =
         KeyBoard.GetKeyDown (ConsoleKey.Q)
 
+    (* Returns which direction player should move *)
+    let Direction () =
+        if KeyBoard.GetKeyDown (ConsoleKey.RightArrow) then 0
+        elif KeyBoard.GetKeyDown (ConsoleKey.DownArrow) then 1
+        elif KeyBoard.GetKeyDown (ConsoleKey.LeftArrow) then 2
+        elif KeyBoard.GetKeyDown (ConsoleKey.UpArrow) then 3
+        else -1
+
 module Game =
-    let rec MainLoop() =
+    let rec MainLoop(snake: (int*int) list, dir) =
+        (* Snake game logic *)
+        (* Move snake *)
+        let snakeFront = snake[0]
+        let (sfx, sfy) = snakeFront
+        let mutable newSnake = snake
+        match dir with
+        | 0 -> newSnake <- ListTools.ReplaceFront (sfx + 1, sfy) newSnake
+        | 1 -> newSnake <- ListTools.ReplaceFront (sfx, sfy + 1) newSnake
+        | 2 -> newSnake <- ListTools.ReplaceFront (sfx - 1, sfy) newSnake
+        | _ -> newSnake <- ListTools.ReplaceFront (sfx, sfy - 1) newSnake
+
+        (* Draw everything *)
         Console.FruitAtLocation (1,1)
-        let snake = [(5,5); (5,6); (6,6); (6,7); (6,8); (7,8); (8,8); (8,7); (7,7)]
-        Console.DrawSnake snake
+        
+        Console.ClearOldSnake snake[snake.Length-1]
+        Console.DrawSnake newSnake
+
+        (* Sleep before next update *)
+        Threading.Thread.Sleep(1000)
+
+        (* If exit button is not pressed, then call MainLoop *)
         let exit = Input.ShouldExit()
-        if not exit then MainLoop()
+        if not exit then 
+            (* Update snake direction *)
+            let sd = Input.Direction()
+            if sd = -1 then
+                MainLoop(newSnake, dir)
+            else
+                MainLoop(newSnake, sd)
 
 [<EntryPoint>]
 let main args =
     ConsoleInterface.ClearScreen
-    Console.DrawBorder(10, 10)
-    Game.MainLoop()
+    Console.DrawBorder(13, 13)
+    Game.MainLoop ([(7,6); (6,6); (5,6)], 0)
     0
